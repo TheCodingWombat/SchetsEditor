994245cb0af527b71b0e0a029789f420904014c3
Xavier <33129471+XavierdeBondt@users.noreply.github.com>
2017-11-06 10:58:13 +0100

    aparte methodes voor berekenLijn en berekenEllipse.

--- SchetsEditor/TekenElement.cs
+++ SchetsEditor/TekenElement.cs
@@ -6,9 +6,9 @@ namespace SchetsEditor
 {
     class TekenElement
     {
-        public Point beginPunt, eindPunt;
-        public Brush kwast; //moet allemaal nog private worden
-        public ISchetsTool tool;
+        private Point beginPunt, eindPunt;
+        private Brush kwast; 
+        private ISchetsTool tool;
 
         public TekenElement(ISchetsTool tool, Point beginPunt, Point eindPunt, Brush kwast)
         {
@@ -27,43 +27,16 @@ namespace SchetsEditor
                     Rectangle myRectangle = TweepuntTool.Punten2Rechthoek(beginPunt, eindPunt);
                     return myRectangle.Contains(p);
                 case "rondje":
-                    Rectangle myEllipse = TweepuntTool.Punten2Rechthoek(beginPunt, eindPunt);
-                    double h = (Math.Abs(eindPunt.X+beginPunt.X) / 2);
-                    double k = (Math.Abs(eindPunt.Y+beginPunt.Y) / 2);
-                    double rx = myEllipse.Width/2;
-                    double ry = myEllipse.Height/2;
-                    double berekening = ((((p.X - h)*(p.X - h)) / (rx*rx)) + (((p.Y - k)*(p.Y - k)) / (ry*ry))) ;
-                    if(berekening <= 1.1)
+                    if(berekenEllipse(p) <= 1.1)
                         return true;
                     return false;
-                case "lijn": //Met behulp van https://autohotkey.com/board/topic/60656-calculate-the-distance-between-a-point-and-a-line-segment/
-                    double px = eindPunt.X - beginPunt.X;
-                    double py = eindPunt.Y - beginPunt.Y;
-                    double u = ((p.X - beginPunt.X)*px + (p.Y-beginPunt.Y)*py)/(px*px + py*py);
-                    if(u > 1) u = 1;
-                    Console.WriteLine("u: "+u);
-                    double x = beginPunt.X + u*px;
-                    double y = beginPunt.Y + u*py;
-                    double dx = x - p.X;
-                    double dy = y - p.Y;
-                    Console.WriteLine(dx*dx + dy*dy);
-                    if((dx*dx + dy*dy)<100) return true;
+                case "lijn": 
+                    if(berekenLijn(p)<100) return true;
                     return false;
                 case "cirkel":
-
-                    Rectangle myEllipseOpen = TweepuntTool.Punten2Rechthoek(beginPunt, eindPunt);
-                    h = (Math.Abs(eindPunt.X+beginPunt.X) / 2);
-                    k = (Math.Abs(eindPunt.Y+beginPunt.Y) / 2);
-                    rx = myEllipseOpen.Width/2;
-                    ry = myEllipseOpen.Height/2;
-                    berekening = ((((p.X - h)*(p.X - h)) / (rx*rx)) + (((p.Y - k)*(p.Y - k)) / (ry*ry))) ;
-                    Console.WriteLine("r_x " + rx + " r_y " + ry + " k " + k + " h " + h);
-                    Console.WriteLine(((((p.X - h)*(p.X - h)) / (rx*rx)) + (((p.Y - k)*(p.Y - k)) / (ry*ry)) == 1));
-
-                    if(berekening < 1.5 && berekening > 0.5 )
+                    if(berekenEllipse(p) < 1.5 && berekenEllipse(p) > 0.5 )
                         return true;
                     return false;
-
                 case "kader":
                     if ((beginPunt.Y - 5 <= p.Y  && beginPunt.Y + 5 >= p.Y ) && ((beginPunt.X <= p.X && eindPunt.X >= p.X ) || (beginPunt.X >= p.X + 5 || eindPunt.X <= p.X - 5)))
                         return true;
@@ -74,24 +47,35 @@ namespace SchetsEditor
                     if ((beginPunt.Y <= p.Y + 5 && eindPunt.Y >= p.Y + 5) && (eindPunt.X <= p.X + 5 && eindPunt.X >= p.X - 5))
                         return true;
                     return false;
-                case "pen": //Met behulp van https://autohotkey.com/board/topic/60656-calculate-the-distance-between-a-point-and-a-line-segment/
-                    px = eindPunt.X - beginPunt.X;
-                    py = eindPunt.Y - beginPunt.Y;
-                    u = ((p.X - beginPunt.X)*px + (p.Y-beginPunt.Y)*py)/(px*px + py*py);
-                    if(u > 1) u = 1;
-                    Console.WriteLine("u: " + u);
-                    x = beginPunt.X + u*px;
-                    y = beginPunt.Y + u*py;
-                    dx = x - p.X;
-                    dy = y - p.Y;
-                    Console.WriteLine(dx*dx + dy*dy);
-                    if((dx*dx + dy*dy)<100) return true;
+                case "pen": 
+                    if(berekenLijn(p)<100) 
+                        return true;
                     return false;
                 
                 default:
                     return false;
             }
-            
+        }
+        private double berekenLijn(Point p)
+        {   //Met behulp van https://autohotkey.com/board/topic/60656-calculate-the-distance-between-a-point-and-a-line-segment/
+            double px = eindPunt.X - beginPunt.X;
+            double py = eindPunt.Y - beginPunt.Y;
+            double u = ((p.X - beginPunt.X)*px + (p.Y-beginPunt.Y)*py)/(px*px + py*py);
+            if(u > 1) u = 1;
+            double x = beginPunt.X + u*px;
+            double y = beginPunt.Y + u*py;
+            double dx = x - p.X;
+            double dy = y - p.Y;
+            return (dx*dx + dy*dy);
+        }
+        private double berekenEllipse(Point p)
+        {   //Met behulp van https://math.stackexchange.com/questions/76457/check-if-a-point-is-within-an-ellipse
+            Rectangle myEllipse = TweepuntTool.Punten2Rechthoek(beginPunt, eindPunt);
+            double h = (Math.Abs(eindPunt.X+beginPunt.X) / 2);
+            double k = (Math.Abs(eindPunt.Y+beginPunt.Y) / 2);
+            double rx = myEllipse.Width/2;
+            double ry = myEllipse.Height/2;
+            return ((((p.X - h)*(p.X - h)) / (rx*rx)) + (((p.Y - k)*(p.Y - k)) / (ry*ry))) ;
         }
     }
 }
